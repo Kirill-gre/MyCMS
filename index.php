5cc75334df7d36a787e95678efa06b503ff3c170
Kirill-gre <kiryane@yandex.ru>
2017-01-19 02:25:47 +0300

    Spaces

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+	//echo 'INDEX';
+
+//phpinfo();
+	require_once("datebase.php");
+	require_once("models/articles.php");
+	//$article = articles_get($_GET['id']);
+		$articles = articles_all();
+
+		include("views/articles.php");
+	
+
+	
+	?>
